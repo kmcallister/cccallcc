a55691a4d4e6e263e156e79ca21c5ecc199a6a25
Keegan McAllister <mcallister.keegan@gmail.com>
2012-02-10 01:44:03 -0500

    move cont<T>::call_cc definition out of class body

--- cccallcc.hpp
+++ cccallcc.hpp
@@ -7,27 +7,7 @@
 template <typename T>
 class cont {
 public:
-    static T call_cc(std::function<T (cont<T>)> f) {
-        int fd[2];
-        pipe(fd);
-        int read_fd  = fd[0];
-        int write_fd = fd[1];
-
-        pid_t pid = fork();
-        if (pid) {
-            // parent
-            close(read_fd);
-            return f( cont<T>(write_fd) );
-        } else {
-            // child
-            close(write_fd);
-            char buf[sizeof(T)];
-            if (read(read_fd, buf, sizeof(T)) < sizeof(T))
-                exit(0);
-            close(read_fd);
-            return *reinterpret_cast<T*>(buf);
-        }
-    }
+    static T call_cc(std::function<T (cont<T>)> f);
 
     void operator()(const T &x) {
         write(m_pipe, &x, sizeof(T));
@@ -40,6 +20,29 @@ private:
 
     int m_pipe;
 };
+
+template <typename T>
+T cont<T>::call_cc(std::function<T (cont<T>)> f) {
+    int fd[2];
+    pipe(fd);
+    int read_fd  = fd[0];
+    int write_fd = fd[1];
+
+    pid_t pid = fork();
+    if (pid) {
+        // parent
+        close(read_fd);
+        return f( cont<T>(write_fd) );
+    } else {
+        // child
+        close(write_fd);
+        char buf[sizeof(T)];
+        if (read(read_fd, buf, sizeof(T)) < sizeof(T))
+            exit(0);
+        close(read_fd);
+        return *reinterpret_cast<T*>(buf);
+    }
+}
 
 template <typename T>
 static inline T call_cc(std::function<T (cont<T>)> f) {
