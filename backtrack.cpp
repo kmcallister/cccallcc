f8ae12c681923876a3722ad18bf5ee6737871ab6
Keegan McAllister <mcallister.keegan@gmail.com>
2012-02-10 23:49:43 -0500

    add backtracking example

---
+++ backtrack.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <boost/optional.hpp>
+
+#include "cccallcc_fork.hpp"
+
+boost::optional< cont<bool> > checkpoint;
+
+bool guess() {
+    return call_cc<bool>( [](cont<bool> k) {
+        checkpoint = k;
+        return true;
+    } );
+}
+
+void fail() NORETURN;
+void fail() {
+    if (checkpoint) {
+        (*checkpoint)(false);
+    } else {
+        std::cerr << "Nothing to be done." << std::endl;
+        exit(1);
+    }
+}
+
+int integer(int m, int n) {
+    for (int i=m; i<=n; i++) {
+        if (guess())
+            return i;
+    }
+    fail();
+}
+
+void factor(int n) {
+    int i = integer(2, 100);
+    int j = integer(2, 100);
+
+    if (i*j != n)
+        fail();
+
+    std::cout << i << " * " << j << " = " << n << std::endl;
+}
+
+const int maze_size = 15;
+char maze[] =
+    "X-------------+\n"
+    "        |     |\n"
+    "|--+  | |   | |\n"
+    "|  |  | | --+ |\n"
+    "|     |     | |\n"
+    "|-+---+--+- | |\n"
+    "| |      |    |\n"
+    "| | | ---+-+- |\n"
+    "|   |      |  |\n"
+    "| +-+-+--|    |\n"
+    "| |   |  |--- |\n"
+    "|     |       |\n"
+    "|--- -+-------|\n"
+    "|              \n"
+    "+------------- \n";
+
+void solve_maze() {
+    int x=0, y=0;
+
+    while ((x != maze_size-1)
+        || (y != maze_size-1)) {
+
+             if (guess()) x++;
+        else if (guess()) x--;
+        else if (guess()) y++;
+        else              y--;
+
+        if (  (x < 0) || (x >= maze_size) ||
+              (y < 0) || (y >= maze_size)  )
+            fail();
+
+        int i = y*(maze_size+1) + x;
+        if (maze[i] != ' ')
+            fail();
+        maze[i] = 'X';
+    }
+
+    for (char c : maze) {
+        if (c == 'X')
+            std::cout << "\e[1;32mX\e[0m";
+        else
+            std::cout << c;
+    }
+}
+
+int main() {
+    factor(391);
+    solve_maze();
+    return 0;
+}
